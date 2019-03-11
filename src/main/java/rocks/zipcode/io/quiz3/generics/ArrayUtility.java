e33a425d037dae711454f7acc875a966298311b0
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-03-11 13:17:40 -0400

    finished most of generics; finished enums

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -1,30 +1,55 @@
 package rocks.zipcode.io.quiz3.generics;
 
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * @author leon on 09/12/2018.
  */
 public class ArrayUtility<SomeType> {
     private final SomeType[] array;
+    private final List<SomeType> arrayList;
+    private final List<SomeType> arrayListUnique;
+    private final Set<SomeType> hashSet;
 
     public ArrayUtility(SomeType[] array) {
         this.array = array;
+        this.arrayList = Arrays.asList(array);
+        hashSet = new LinkedHashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            hashSet.add(array[i]);
+        }
+        arrayListUnique = new ArrayList<>(hashSet);
     }
 
     public SomeType findOddOccurringValue() {
-        return null;
+        SomeType st = null;
+        for (int i = 0; i < arrayListUnique.size(); i++) {
+            if (Collections.frequency(arrayList, arrayListUnique.get(i)) % 2 != 0) {
+                st = arrayListUnique.get(i);
+            }
+        }
+        return st;
     }
 
     public SomeType findEvenOccurringValue() {
-        return null;
+        SomeType st = null;
+        for (int i = 0; i < arrayListUnique.size(); i++) {
+            if (Collections.frequency(arrayList, arrayListUnique.get(i)) % 2 == 0) {
+                st = arrayListUnique.get(i);
+            }
+        }
+        return st;
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-        return null;
+        return Collections.frequency(arrayList, valueToEvaluate);
     }
 
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
-        return null;
+//        return Arrays.stream(array)
+//                .filter(Sometype -> ;
+                return null;
     }
 }
